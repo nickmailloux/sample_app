c842426eb2875454a6acf6b5cd142988820386fc
Nick Mailloux <nickmailloux@gmail.com>
2014-01-15 20:50:03 -0500

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   before_create :create_remember_token
   before_save { email.downcase! }
   attr_accessible :name, :email, :password, :password_confirmation, :admin
+  has_many :microposts, dependent: :destroy
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence:   true, 
@@ -16,6 +17,10 @@ class User < ActiveRecord::Base
   
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
+  end
+  
+  def feed
+    Micropost.where("user_id = ?", id)
   end
   
   private
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -66,7 +66,9 @@ describe "Authentication" do
             before do
               delete signout_path
               visit signin_path
-              valid_signin(user)
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
             end
             
             it "should render the default (profile) page" do
@@ -93,6 +95,19 @@ describe "Authentication" do
 				  before { visit users_path }
 				  it { should have_title('Sign in') }
 				end
+			end
+			
+			describe "in the Microposts controller" do
+			  
+			  describe "submitting to the create action" do
+			    before { post microposts_path }
+			    specify { expect(response).to redirect_to(signin_path) }
+			  end
+			  
+			  describe "submitting to the destroy action" do
+			    before { delete micropost_path(FactoryGirl.create(:micropost) ) }
+			    specify { expect(response).to redirect_to(signin_path) }
+			  end
 			end
     end
     
